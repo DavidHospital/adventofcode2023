505ef89a82f24beba72aa5e9b1dfcffe71016f10
frosty <hospitald27@gmail.com>
2023-12-06 13:53:33 -0500

    finished this crazy puzzle somehow

--- d5/5b.py
+++ d5/5b.py
@@ -11,6 +11,18 @@ def range_overlap(s1: int, r1: int, s2: int, r2: int) -> Tuple[int, int]:
     return (start, rnge)
 
 
+def range_diff(s1: int, r1: int, s2: int, r2: int) -> List[Tuple[int, int]]:
+    result = []
+    overlap = range_overlap(s1, r1, s2, r2)
+    if overlap[1] <= 0:
+        return [(s1, r1)]
+    if overlap[0] > s1:
+        result.append((s1, overlap[0] - s1))
+    if s1 + r1 > overlap[0] + overlap[1]:
+        result.append((overlap[0] + overlap[1], s1 + r1 - overlap[0] - overlap[1]))
+    return result
+
+
 def lookup(value: int, key: str = "seed", dest: str = "") -> int:
     while key != dest and key in almanac_map:
         value = next((v[0] + (value - src) for src, v in almanac_map[key].items() if src != "maps" and value >= src and value < src + v[1]), value)
@@ -19,17 +31,29 @@ def lookup(value: int, key: str = "seed", dest: str = "") -> int:
 
 
 def lookup_range(start: int, rnge: int, key: str = "seed", dest: str = "") -> List[Tuple[int, int]]:
-    dest_ranges = []
-    for src, v in almanac_map[key].items():
-        if src == "maps":
-            continue
-        overlap = range_overlap(start, rnge, src, v[1])
-        if overlap[1] > 0:
-            dest_ranges.append(overlap)
-    return []
+    input_ranges = [(start, rnge)]
+    while key != dest and key in almanac_map:
+        dest_ranges = []
+        for (start, rnge) in input_ranges:
+            left_overs = [(start, rnge)]
+            for src, v in almanac_map[key].items():
+                if src == "maps":
+                    continue
+                overlap = range_overlap(start, rnge, src, v[1])
+                if overlap[1] > 0:
+                    dest_ranges.append((overlap[0] - src + v[0], overlap[1]))
+                    new_left_overs = []
+                    for lo in left_overs:
+                        new_left_overs += range_diff(lo[0], lo[1], overlap[0], overlap[1])
+                    left_overs = new_left_overs
+
+            dest_ranges += left_overs
+        input_ranges = dest_ranges
+        key = almanac_map[key]["maps"]
+    return input_ranges
 
 
-with open("example.txt", "r") as f:
+with open("input.txt", "r") as f:
     lines = f.readlines()
 
     seeds = [int(seed.strip()) for seed in lines[0].split(":")[1].split()]
@@ -61,6 +85,8 @@ it = iter(seeds)
 seed_ranges = [(x, next(it)) for x in it]
 # print(seed_ranges)
 
+ranges = []
 for rnge in seed_ranges:
-    print(lookup_range(rnge[0], rnge[1]))
-    
+    ranges += lookup_range(rnge[0], rnge[1])
+
+print(min([r[0] for r in ranges]))
